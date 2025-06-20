9cc26ed62604bc5d2edfeacc9e2a53c6b12a6015
kang <dlwprkdlq@naver.com>
2025-06-20 17:41:02 +0900

    Add: BottomNavigation 추가

---
+++ lib/screen/oven_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class OvenScreen extends StatelessWidget {
+  const OvenScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('oven'));
+  }
+}
