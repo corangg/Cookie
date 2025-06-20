671c345e1f10df190f8fcac8c855e34fd8a69323
kang <dlwprkdlq@naver.com>
2025-06-21 02:45:59 +0900

    Chore: Icon 변경

--- packages/core/lib/values/app_size.dart
+++ packages/core/lib/values/app_size.dart
@@ -1,5 +1,5 @@
 class AppSize {
-  static const bottomNavigationBarIcon = 32.0;
+  static const bottomNavigationBarIcon = 24.0;
   static const bottomNavigationBarActiveIcon = 40.0;
-  static const bottomNavigationBarIconRadius = 32.0;
+  static const bottomNavigationBarIconRadius = 40.0;
 }
