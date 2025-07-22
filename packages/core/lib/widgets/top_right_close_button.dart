958a0246977ba0aef358ca3c40eae7b72b652389
kang <dlwprkdlq@naver.com>
2025-07-23 03:20:00 +0900

    Chore: 구성 요소 수정 가능하게 변경

--- packages/core/lib/widgets/top_right_close_button.dart
+++ packages/core/lib/widgets/top_right_close_button.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
 class TopRightCloseButton extends StatelessWidget {
+  final double iconSize;
+  final double iconTop;
+  final double iconRight;
+  final Color iconColor;
   final VoidCallback? onTap;
 
   const TopRightCloseButton({
     super.key,
+    this.iconSize = 32,
+    this.iconTop = 24,
+    this.iconRight = 24,
+    this.iconColor = Colors.white,
     this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      top: 24,
-      right: 24,
+      top: iconTop,
+      right: iconRight,
       child: GestureDetector(
         onTap: onTap,
-        child: const Icon(Icons.close, size: 32, color: Colors.white),
+        child: Icon(Icons.close, size: iconSize, color: iconColor),
       ),
     );
   }
